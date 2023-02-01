0679be66883cad9ab4a9e25dcfc4dae6401aa456
“Vssandip” <sandipshindesr95035@gmail.com>
2023-02-01 21:27:00 +0530

    [ SANDIP ] Adding  'Binary Search Tree (BST2)'

--- Binary-Search-Tree-BST/Program.cs
+++ Binary-Search-Tree-BST/Program.cs
@@ -5,9 +5,24 @@
         static void Main(string[] args)
         {
             Console.WriteLine(" ***** Welcome Binary-Search-Tree-BST ***** ");
+            /*  BST<int> bst = new BST<int>(56);
+              bst.add(30);
+              bst.add(70);
+              bst.show();*/
+
             BST<int> bst = new BST<int>(56);
             bst.add(30);
             bst.add(70);
+            bst.add(22);
+            bst.add(40);
+            bst.add(11);
+            bst.add(3);
+            bst.add(16);
+            bst.add(60);
+            bst.add(95);
+            bst.add(65);
+            bst.add(63);
+            bst.add(67);
             bst.show();
         }
     }
